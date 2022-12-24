064c7b378f984a9c2d5126f66038df49b2aa6148
Lmntria <seymur1095@gmail.com>
2022-12-25 02:43:21 +0400

    Slider finished in admin Panel

--- QuarterApp/QuarterApp/Areas/Manage/Controllers/SliderController.cs
+++ QuarterApp/QuarterApp/Areas/Manage/Controllers/SliderController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuarterApp.DAL;
+using QuarterApp.Helpers;
+using QuarterApp.Models;
 
 namespace QuarterApp.Areas.Manage.Controllers
 {
@@ -7,30 +9,126 @@ namespace QuarterApp.Areas.Manage.Controllers
     public class SliderController : Controller
     {
         private readonly QuarterDbContext _context;
-        private readonly IWebHostEnvironment _web;
+        private readonly IWebHostEnvironment _env;
 
-        public SliderController(QuarterDbContext context,IWebHostEnvironment web)
+        public SliderController(QuarterDbContext context,IWebHostEnvironment env)
         {
             _context= context;
-            _web = web;
+            _env = env;
         }
         public IActionResult Index()
         {
             var model = _context.Sliders.OrderBy(x => x.Order).ToList();
 
+
+
             return View(model);
         }
         public IActionResult Create()
         {
+            var slider = _context.Sliders.OrderByDescending(x => x.Order).FirstOrDefault();
+            int order = slider == null ? 1 : slider.Order + 1;
+            ViewBag.Order = order;
             return View();
         }
-        public IActionResult Edit()
+        [HttpPost]
+        public IActionResult Create(Slider slider)
         {
-            return View();
+            if (slider.ImageFile != null && slider.ImageFile.ContentType != "image/png" && slider.ImageFile.ContentType != "image/jpeg" && slider.ImageFile.ContentType != "image/jpg")
+            {
+                ModelState.AddModelError("ImageFile", "Content type must be image/png or image/jpeg or image/jpg");
+                return View();
+            }
+
+            if(slider.ImageFile != null && slider.ImageFile.Length> 3145728)
+            {
+                ModelState.AddModelError("ImageFile", "ImageFile size must be lower than 3MB");
+                return View();
+            }
+
+
+
+            if (!ModelState.IsValid)
+                return View();
+
+
+            slider.Image = FileManager.Save(slider.ImageFile, _env.WebRootPath, "uploads/sliders");
+
+            _context.Sliders.Add(slider);
+            _context.SaveChanges();
+
+
+            return RedirectToAction("Index");
         }
-        public IActionResult Delete()
+        public IActionResult Edit(int id)
         {
-            return View();
+            var slider=_context.Sliders.FirstOrDefault(x=>x.Id == id);
+
+            if (slider == null)
+                return RedirectToAction("error", "dashboard");
+
+            return View(slider);
+        }
+        [HttpPost]
+        public IActionResult Edit(Slider slider)
+        {
+            if (slider.ImageFile != null && slider.ImageFile.ContentType != "image/png" && slider.ImageFile.ContentType != "image/jpeg" && slider.ImageFile.ContentType != "image/jpg")
+            {
+                ModelState.AddModelError("ImageFile", "Content type must be image/png or image/jpeg or image/jpg");
+                return View();
+            }
+
+            if (slider.ImageFile != null && slider.ImageFile.Length > 3145728)
+            {
+                ModelState.AddModelError("ImageFile", "ImageFile size must be lower than 3MB");
+                return View();
+            }
+
+            if (!ModelState.IsValid)
+                return View();
+
+            Slider existedSlider = _context.Sliders.FirstOrDefault(x=>x.Id==slider.Id);
+
+            if (existedSlider == null)
+                return RedirectToAction("error", "dashboard");
+
+            if (slider.ImageFile != null)
+            {
+                var newImageName = FileManager.Save(slider.ImageFile, _env.WebRootPath, "uploads/sliders");
+
+                FileManager.Delete(_env.WebRootPath,"uploads/sliders",existedSlider.Image);
+
+                existedSlider.Image= newImageName;
+            }
+
+
+            existedSlider.Order = slider.Order;
+            existedSlider.Tittle1 = slider.Tittle1;
+            existedSlider.Tittle2 = slider.Tittle2;
+            existedSlider.Desc = slider.Desc;
+            existedSlider.FirstBtnText = slider.FirstBtnText;
+            existedSlider.FirstBtnUrl = slider.FirstBtnUrl;
+            existedSlider.SecondBtnText = slider.SecondBtnText;
+            existedSlider.SecondBtnUrl = slider.SecondBtnUrl;
+            existedSlider.FirstBtnUrl = slider.FirstBtnUrl;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var slider=_context.Sliders.FirstOrDefault(x=>x.Id== id);
+
+            if (slider == null)
+                return RedirectToAction("error", "dashboard");
+
+            FileManager.Delete(_env.WebRootPath, "uploads/sliders", slider.Image);
+
+            _context.Sliders.Remove(slider);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
--- QuarterApp/QuarterApp/Models/Slider.cs
+++ QuarterApp/QuarterApp/Models/Slider.cs
@@ -9,24 +9,24 @@ namespace QuarterApp.Models
         public int Id { get; set; }
         [MaxLength(100)]
         public string Tittle1 { get; set; }
-		[MaxLength(100)]
-		public string Tittle2 { get; set; }
-		[MaxLength(250)]
-		public string Desc { get; set; }
-		[MaxLength(80)]
-		public string FirstBtnText { get; set; }
-		[MaxLength(100)]
-		public string FirstBtnUrl { get; set; }
-		[MaxLength(80)]
-		public string? SecondBtnText { get; set; }
-		[MaxLength(100)]
-		public string? SecondBtnUrl { get; set;}
-		[MaxLength(100)]
-		public string? Image { get; set; }
+        [MaxLength(100)]
+        public string Tittle2 { get; set; }
+        [MaxLength(250)]
+        public string Desc { get; set; }
+        [MaxLength(80)]
+        public string FirstBtnText { get; set; }
+        [MaxLength(100)]
+        public string FirstBtnUrl { get; set; }
+        [MaxLength(80)]
+        public string? SecondBtnText { get; set; }
+        [MaxLength(100)]
+        public string? SecondBtnUrl { get; set; }
+        [MaxLength(100)]
+        public string? Image { get; set; }
         public byte Order { get; set; }
-		[NotMapped]
-		[AllowedFileType("image/png", "image/jpeg")]
-		[MaxFileSize(3)]
+        [NotMapped]
+        [MaxFileSize(3)]
+        //[AllowedFileType("image/png", "image/jpeg", "image/jpg")]
         public IFormFile? ImageFile { get; set; }
     }
 }
