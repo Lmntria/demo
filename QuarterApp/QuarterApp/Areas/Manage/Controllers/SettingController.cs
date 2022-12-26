5a4d75afdfdae376db17d80e22eb804469dab993
Lmntria <seymur1095@gmail.com>
2022-12-27 02:05:20 +0400

    Amenity and HouseAmenity and Setting added to admin panel

---
+++ QuarterApp/QuarterApp/Areas/Manage/Controllers/SettingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuarterApp.DAL;
+
+namespace QuarterApp.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class SettingController : Controller
+    {
+        private readonly QuarterDbContext _context;
+        public SettingController(QuarterDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var model = _context.Settings.ToList();
+
+            return View(model);
+        }
+    }
+}
